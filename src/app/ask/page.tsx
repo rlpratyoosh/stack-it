0a1252522055aa435e6c088e4bb28c7658df872d
rlpratyoosh <pratyoosh.prakash.dev@gmail.com>
2025-07-12 09:23:38 +0530

    Adding Routes to the App

---
+++ src/app/ask/page.tsx
@@ -0,0 +1,8 @@
+export default function AskQuestion() {
+  return (
+    <>
+      <h1>Ask a Question</h1>
+      <p>This page allows users to ask new questions.</p>
+    </>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 export default function Home() {
   return (
   <>
+  This page shows all the questions.
   </>
    );
 }
