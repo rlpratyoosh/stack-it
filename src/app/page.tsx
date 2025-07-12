6f259a8546b6f77874e6a1f4a1e606200a58e940
rlpratyoosh <pratyoosh.prakash.dev@gmail.com>
2025-07-12 10:02:41 +0530

    Solving merge conflicts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-<<<<<<< HEAD
-    <> 
-      <main className="min-h-screen">
-        {/* Your page content goes here */}
-      </main>
-    </>
-  );
-=======
   <>
-  This page shows all the questions.
+    This page shows all the questions.
   </>
    );
->>>>>>> 7acd854cef9c4fd08a4e6f7ec42f1b42a9a6d4f2
 }
